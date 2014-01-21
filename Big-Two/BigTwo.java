d162a97c4c197376347e1bb6b7f943aede79a486
Yiwei Da <dayiwei@hotmail.com>
2014-01-21 02:04:30 -0500

    Create players and deck

--- Big-Two/BigTwo.java
+++ Big-Two/BigTwo.java
@@ -1,6 +1,12 @@
 public class BigTwo {
 
     public void play() {
+	Arraylist<Card> deck = new ArrayList<Card>();
+	for(int i=1;i<5;i++)
+	    for (int x=1; x<14;x++){
+		Card c = new Card(x,i);
+		deck.add(c);
+	    }
 
 	ArrayList<Card> player1 = new ArrayList<Card>();
 	ArrayList<Card> player2 = new ArrayList<Card>();

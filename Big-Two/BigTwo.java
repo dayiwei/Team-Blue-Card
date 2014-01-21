c48e18dd5f4239a985233836acae9ddcc3a4c891
Yiwei Da <dayiwei@hotmail.com>
2014-01-21 01:44:58 -0500

    Start on play()

---
+++ Big-Two/BigTwo.java
@@ -0,0 +1,13 @@
+public class BigTwo {
+
+    public void play() {
+
+	ArrayList<Card> player1 = new ArrayList<Card>();
+	ArrayList<Card> player2 = new ArrayList<Card>();
+	ArrayList<Card> player3 = new ArrayList<Card>();
+	ArrayList<Card> player4 = new ArrayList<Card>();
+	
+    }
+    
+    
+}

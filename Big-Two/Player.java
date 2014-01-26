f76e26fd96f908953e0460dc9d9f7cfd911fdcc1
Shein Htike <chocolateshein.htike@gmail.com>
2014-01-26 18:47:14 -0500

    So you want a Player.

--- Big-Two/Player.java
+++ Big-Two/Player.java
@@ -1,24 +1,39 @@
 import java.util.ArrayList;
 
-public class Player {
+public class Player extends ArrayList<Card>{
 
     private String name;
-    private ArrayList<Card> hand;
-
+    private boolean won;
     public Player(String playername) {
-
-	name = playername;
-	hand = new ArrayList<Card>();
-
+        super();
+        name = playername;
+        won = false;
     }
-
+    public ArrayList<Card> getHand(){
+        return (ArrayList<Card>) this;
+    }
     public String getName(){return name;}
-
-    public ArrayList<Card> getHand(){return hand;}
-
-    public void addCard(Card c){
-	hand.add(c);
-
+    public Combo play(ArrayList<Card> x){
+        for(Card y : x){
+            if(contains(y)){
+                remove(indexOf(y));
+            }
+        }
+        return new Combo(x);
     }
-
+    public String toString(){
+        String ret = "";
+        ret += name + "'s hand:\n";
+        for(int count = 0; count < size(); count++){
+            ret += Integer.toString(count + 1) + ". " + get(count) + "\n";
+        }
+        return ret.substring(0,ret.length() - 1);
+    }
+    public static void main(String[] args){
+        Player a = new Player("Shein");
+        for(int x = 1; x <= 13; x++){
+            a.add(new Card(x,4));
+        }
+        System.out.print(a);
+    }
 }

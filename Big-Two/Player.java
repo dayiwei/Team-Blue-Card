5a50e00dc644bb89fc5cc2f90ab22c2c95e98c93
Shein Htike <chocolateshein.htike@gmail.com>
2014-01-29 23:57:19 -0500

    Changes to Player

--- Big-Two/Player.java
+++ Big-Two/Player.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
 
 public class Player extends ArrayList<Card>{
-
-    private String name;
     private boolean won;
+    private String name;
     public Player(String playername) {
         super();
         name = playername;
@@ -13,27 +12,39 @@ public class Player extends ArrayList<Card>{
         return (ArrayList<Card>) this;
     }
     public String getName(){return name;}
-    public Combo play(ArrayList<Card> x){
+    public Trick play(ArrayList<Card> x){
         for(Card y : x){
             if(contains(y)){
                 remove(indexOf(y));
             }
         }
-        return new Combo(x);
+        return new Trick(x);
+    }
+    public boolean won(){
+        return won;
+    }
+    public void checkwin(){
+        if (size() == 0){
+            won = true;
+        }
+        else if(size() == 13 && Trick.sameSuit(this)){
+            won = true;
+        }
     }
     public String toString(){
-        String ret = "";
-        ret += name + "'s hand:\n";
-        for(int count = 0; count < size(); count++){
-            ret += Integer.toString(count + 1) + ". " + get(count) + "\n";
-        }
-        return ret.substring(0,ret.length() - 1);
+        String ret = "Your Cards:\n";
+        ret += GraphicCards.toString(this);
+        return ret;
     }
     public static void main(String[] args){
         Player a = new Player("Shein");
-        for(int x = 1; x <= 13; x++){
+        for(int x = 13; x >= 1; x--){
             a.add(new Card(x,4));
         }
-        System.out.print(a);
+        System.out.println(a);
+        ArrayList<Card> b = new ArrayList<Card>();
+        b.add(new Card(1,4));
+        System.out.println(b);
+        System.out.println(a.play(b));
     }
 }
